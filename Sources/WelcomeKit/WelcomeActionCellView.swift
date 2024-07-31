c03b7fc617a98afbae83c9af80d63b9358660fc0
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2024-07-31 23:00:00 +0800

    Updates

--- Sources/WelcomeKit/WelcomeActionCellView.swift
+++ Sources/WelcomeKit/WelcomeActionCellView.swift
@@ -2,30 +2,37 @@ import AppKit
 
 class WelcomeActionCellView: TableCellView {
     let iconImageView = NSImageView()
-    
+
     let titleLabel = NSTextField(labelWithString: "")
 
     let detailLabel = NSTextField(labelWithString: "")
 
     init() {
         super.init(frame: .zero)
+
         addSubview(iconImageView)
         addSubview(titleLabel)
         addSubview(detailLabel)
-        
+
         iconImageView.makeConstraints { make in
             make.leftAnchor.constraint(equalTo: leftAnchor)
             make.centerYAnchor.constraint(equalTo: centerYAnchor)
             make.widthAnchor.constraint(equalToConstant: 35)
             make.heightAnchor.constraint(equalToConstant: 35)
         }
+        
         titleLabel.makeConstraints { make in
             make.leftAnchor.constraint(equalTo: iconImageView.rightAnchor, constant: 15)
             make.topAnchor.constraint(equalTo: topAnchor, constant: 9)
         }
+        
         detailLabel.makeConstraints { make in
             make.leftAnchor.constraint(equalTo: titleLabel.leftAnchor)
             make.topAnchor.constraint(equalTo: titleLabel.bottomAnchor)
+        }
+        
+        iconImageView.do {
+            $0.contentTintColor = .controlAccentColor
         }
     }
 }
